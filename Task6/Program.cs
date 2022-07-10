aae15b65383bd1bf1044f31390a71894fbd73cc2
Gavrilyuk <gavrilyukii@mail.ru>
2022-07-10 20:04:43 +0300

    Выполнено домашнее задание номер 6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    if (num%2==0)
+{
+    Console.WriteLine("Четное");
+}
+    else
+{
+    Console.WriteLine("Нечетное");
+}
