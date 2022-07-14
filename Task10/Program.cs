a4e43e46222998dd181b107cc25a1ea4557111e2
Gavrilyuk <gavrilyukii@mail.ru>
2022-07-14 16:46:56 +0300

    Выполнено домашнее задание номер 10, дописал очищение консоли

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = num / 10; 
+int res = a % 10; 
+    if (num < 100 || num >= 1000)
+{
+    Console.WriteLine ("Введено некорректное число, попробуйте еще раз");
+}
+    else
+{
+    Console.WriteLine ($"Вторая цифра числа: {res}");    
+}
+
