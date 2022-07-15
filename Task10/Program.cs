1c88bc4e9bff15eb9f2031c7f8b4da9fc54d19a8
Gavrilyuk <gavrilyukii@mail.ru>
2022-07-15 16:21:08 +0300

    Выполнено домашнее задание номер 10 с использованием метода

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,14 +1,16 @@
-﻿Console.Clear();
-Console.WriteLine("Введите трехзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int a = num / 10; 
-int res = a % 10; 
-    if (num < 100 || num >= 1000)
-{
-    Console.WriteLine ("Введено некорректное число, попробуйте еще раз");
-}
-    else
-{
-    Console.WriteLine ($"Вторая цифра числа: {res}");    
-}
+﻿    Console.Clear();
+    Console.WriteLine("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
 
+    if (num < 100 || num >= 1000)
+    {
+        Console.WriteLine("Введено некорректное число, попробуйте еще раз");
+    }
+    else
+    {
+        Console.WriteLine($"Вторая цифра числа: {getSecondDigit(num)}");
+    }
+    int getSecondDigit(int number)
+    {
+        return number / 10 % 10;
+    }
