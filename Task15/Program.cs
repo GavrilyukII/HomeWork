f816326c41284bdd244a9dbbbf898fdada4db274
Gavrilyuk <gavrilyukii@mail.ru>
2022-07-15 16:51:32 +0300

    Выполнено домашнее задание номер 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+    Console.WriteLine("Введите цифру, обозначающую день недели: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num < 1 || num > 7)
+    {
+        Console.WriteLine("Введена некорректная цифра, попробуйте еще раз");
+    }
+    else if (num == 6 || num == 7)
+    {
+        Console.WriteLine($"{num} --> Да");
+    }
+    else
+    {
+        Console.WriteLine($"{num} --> Нет");
+    }
