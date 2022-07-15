d6372f37bad43127643e335d4ede68e886472f4a
Gavrilyuk <gavrilyukii@mail.ru>
2022-07-15 16:28:26 +0300

    Выполнено домашнее задание номер 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    Console.Clear();
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num<100)
+    {
+        Console.WriteLine($"Третьей цифры числа нет");
+        return;
+    }
+    while (num >= 1000) 
+    {   
+        num = num / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {num%10}");
+            
+        
