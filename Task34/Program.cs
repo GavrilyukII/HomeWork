cd7eec727522bca66d53dff6f544cc1a649005a9
Gavrilyuk <gavrilyukii@mail.ru>
2022-08-11 00:49:46 +0300

    выполнена задача номер 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,26 +2,39 @@
 //2.Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] array = new int [8];
-int rndNum = new Random().Next(100,1000);
-int even = 0;
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
+int[] CreateArrayRndInd (int size, int min, int max)
 {
-    array[i] = new Random().Next(100,1000);
+    int[] arr = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
 }
-for (int i = 0; i < array.Length; i++)
-{
-    Console.Write(array[i]);
-    if(i != array.Length -1)
-        Console.Write(", ");
-}
-Console.Write("]");
-for (int i = 0; i < array.Length; i++)
-{
-    //array[i] = new Random().Next(100,1000);
-    if (array[i]%2==0);
-    even++;
 
+int GetCountEvenNum (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0)
+        count++;
+    }
+    return count;
 }
-Console.Write($"-> {even}");
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == 0) Console.Write("[");
+        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "] ");
+    }
+}
+Console.Clear();
+int[] array = CreateArrayRndInd (15, 100, 999);
+PrintArray (array);
+int count = GetCountEvenNum (array);
+Console.Write($"-> {count}");
