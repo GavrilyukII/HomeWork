570750b3909995b107aee87ea20e3f1628a7e57b
Gavrilyuk <gavrilyukii@mail.ru>
2022-08-17 17:50:30 +0300

    выполнена задача номер 52

---
+++ Task52/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+CreateArrayInt (array);
+PrintArray (array);
+Console.WriteLine();
+
+void CreateArrayInt (int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1,50);
+        }
+    }
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int resNumber = array[i, j];
+            Console.Write(resNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine($"Cреднее арифметическое:");
+for (int i = 0; i < n; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += array[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / m, 1);
+  Console.WriteLine($"столбца № {i+1} -> {arithmeticMean}");
+}
