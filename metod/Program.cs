72fba4edc0f9df739ac10db691ab63e9fde2feda
Gavrilyuk <gavrilyukii@mail.ru>
2022-08-18 15:25:21 +0300

    добавлен метод создания двумерного массива

--- metod/Program.cs
+++ metod/Program.cs
@@ -49,7 +49,7 @@ Console.WriteLine();
 Console.WriteLine($"Сумма положительных чисел = {sumPosNegElem[1]}");
 Console.WriteLine($"Сумма отрицательных чисел = {sumPosNegElem[0]}");
 
-void Reverse (int array [])
+void Reverse (int[] array)
 {
     int size = array.Legth;
     int index1 = 0;
@@ -62,5 +62,33 @@ void Reverse (int array [])
 
         index1++;
         index2--;
+    }
+}
+// Задайте двумерный массив и выведите в консоль
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(j == 0) Console.Write("[");
+            if(j < array.GetLength(1) - 1) Console.Write($"{array[i,j], 3},");
+            else Console.Write($"{array[i,j], 3}");
+        }
+        Console.WriteLine();
     }
 }
