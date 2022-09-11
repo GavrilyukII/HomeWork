c4b1b7381a6866df6dec3bfdf974771869d4e920
Gavrilyuk <gavrilyukii@mail.ru>
2022-09-11 20:10:10 +0300

    Создал исходных массив, а также переменную = кол-во искомых значений, попадающих под условия + метод поиска

---
+++ ControlWork/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Clear();
+int CountDigitThree (string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            count++;
+        } 
+    }
+    return count;
+}
+
+string[] array1 = {"hello", "3", "world", ")6GG", "y3y", "K)k45"};
+int count = CountDigitThree (array1);
+
